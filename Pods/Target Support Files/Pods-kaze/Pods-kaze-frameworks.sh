27a303fa49f776c46825a9f60913de4bfebb65ec
Szabolcs Toth <tsz@purzelbaum.hu>
2017-06-18 17:22:33 +0400

    Sparkle implemented.

--- Pods/Target Support Files/Pods-kaze/Pods-kaze-frameworks.sh
+++ Pods/Target Support Files/Pods-kaze/Pods-kaze-frameworks.sh
@@ -90,9 +90,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/HockeySDK-Mac/HockeySDK-Mac/HockeySDK.framework"
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/HockeySDK-Mac/HockeySDK-Mac/HockeySDK.framework"
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
